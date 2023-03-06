731d08ad14bd070e41cb13d9999afb1e7cd37b0f
張秉庭 <u0924031@o365.nuu.edu.tw>
2023-03-06 15:40:21 +0800

    add email helper class

---
+++ app/Helpers/Email.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Helpers;
+
+class Email
+{
+    public static function send($address, $content): string
+    {
+        return "email";
+    }
+}
--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Email' => App\Helpers\Email::class,
     ])->toArray(),
 
 ];
