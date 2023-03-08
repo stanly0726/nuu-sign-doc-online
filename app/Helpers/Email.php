c21bcfd6792b2f15319b3cbb8862089c0f2cf478
張秉庭 <u0924031@o365.nuu.edu.tw>
2023-03-08 17:36:22 +0800

    email helper function

--- app/Helpers/Email.php
+++ app/Helpers/Email.php
@@ -2,10 +2,27 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\Http;
+
 class Email
 {
-    public static function send($address, $content): string
+    public static function send($subject, $content, $recipent, $reply): array
     {
-        return "email";
+        $account = env('EMAIL_API_ACCOUNT');
+        $password = env('EMAIL_API_PASSWORD');
+        $email_api_endpoint = 'http://120.105.144.15/~api/mail/sendmail.php';
+
+        $recipent = json_encode(array($recipent));
+
+        $response = Http::post($email_api_endpoint, [
+            'subject' => $subject,
+            'content' => $content,
+            'recipent' => $recipent,
+            'account' => $account,
+            'password' => $password,
+            'reply' => $reply,
+        ]);
+
+        return $response->json();
     }
 }
